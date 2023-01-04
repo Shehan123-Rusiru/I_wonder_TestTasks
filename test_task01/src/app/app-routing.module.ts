95760d90e4a822633a50d24d2c4708d5725d15a3
Shehan123-Rusiru <ss.rusiru@gmail.com>
2023-01-04 10:58:42 +0530

    Default Path set

--- test_task01/src/app/app-routing.module.ts
+++ test_task01/src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  
-{ path: 'user-component', component: UserComponent },
-{ path: 'home-component', component: HomeComponent },
+
+  { path: 'user-component', component: UserComponent },
+  { path: 'home-component', component: HomeComponent },
+  {
+    path: '',redirectTo:'home-component', pathMatch: 'full'
+  }
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  
+
 })
 export class AppRoutingModule { }
--- test_task01/src/app/app.component.ts
+++ test_task01/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test_task01';
+  currentCustomer = 'Maria';
 }
